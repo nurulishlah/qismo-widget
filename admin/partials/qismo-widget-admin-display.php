0c5c2cba9c0a4fdaa6eb4eabdd497076eb07a7c7
Muhamad Ishlah <nurul.ishlah@qiscus.com>
2019-04-23 12:57:42 +0700

    Add basic settings UI

--- admin/partials/qismo-widget-admin-display.php
+++ admin/partials/qismo-widget-admin-display.php
@@ -14,3 +14,37 @@
 ?>
 
 <!-- This file should primarily consist of HTML with a little bit of PHP. -->
+<div class="wrap">
+    <h1><?php echo esc_html(get_admin_page_title()); ?></h1>
+    <p>
+        <?php
+            _e(
+                'Please provide your Qiscus Multichannel App ID. You can get your App ID from <a href="https://qismo.qiscus.com/settings#information" target="_blank">App Information page</a>',
+                $this->plugin_name
+            );
+        ?>
+    </p>
+    <hr>
+    <form method="post" action="options.php" name="qismo_options">
+        <table class="form-table">
+            <tbody>
+            <tr>
+                <th scope="row">
+                    <label><?php esc_attr_e('App ID', $this->plugin_name); ?></label>
+                </th>
+                <td>
+                    <input type="text" name="qismo_app_id"
+                           id="qismo_app_id"
+                           class="regular-text"
+                           placeholder="Put your app id here ..."
+                           value="<?php
+                           echo (isset($data['app_id'])) ? $data['app_id'] : '';
+                           ?>"/>
+                </td>
+            </tr>
+            </tbody>
+        </table>
+        <?php submit_button('Save changes', 'primary','submit', TRUE); ?>
+
+    </form>
+</div>
